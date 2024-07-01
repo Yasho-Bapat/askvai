b4f6b05dfeb36fc86905601756b8ebf38454a880
Yasho-Bapat <f20210006@goa.bits-pilani.ac.in>
2024-07-01 12:57:13 +0530

    experiment logger

--- ask_viridium_ai/tracking.py
+++ ask_viridium_ai/tracking.py
@@ -16,23 +16,30 @@ class Logger:
         Args:
             info (dict): A dictionary containing information to log.
         """
-        time = info["time"]
-        user_id = info["user_id"]
-        material_name = info["material_name"]
-        tokens_used_for_chemical_composition = info["tokens_used_for_chemical_composition"]
-        cost_chemical_composition = info["cost_chemical_composition"]
-        tokens_used_for_analysis = info["tokens_used_for_analysis"]
-        cost_analysis = info["cost_analysis"]
-        total_cost = info["total_cost"]
-        chemical_composition = info["chemical_composition"]
-        pfas = info["PFAS_status"]
-
-        data = [time, user_id, material_name, tokens_used_for_chemical_composition, cost_chemical_composition,
-                tokens_used_for_analysis, cost_analysis, total_cost, chemical_composition, pfas]
         tdf = pd.DataFrame(info, index=[0])
         self.df = pd.concat([self.df, tdf])
 
-    def save(self):
-        tdf = pd.read_csv("log.csv")
+    def experiment_log(self, info):
+        tdf = pd.DataFrame(info, index=[0])
+
+        self.df = pd.concat([self.df, tdf])
+
+    def save(self, filename):
+        # tdf = pd.read_csv(filename)
+        # self.df = pd.concat([tdf, self.df])
+        self.df.to_csv(filename, index=False)
+
+class ExperimentLogger():
+    def __init__(self):
+        self.columns = ["material_id", "material_name", "manufacturer_id", "manufacturer_name", "pfas_status"]
+        self.df = pd.DataFrame(columns=self.columns)
+        self.logger = Logger()
+
+    def log(self, info):
+        tdf = pd.DataFrame(info, index=[0])
+        self.df = pd.concat([self.df, tdf])
+
+    def save(self, filename):
+        tdf = pd.read_csv(filename)
         self.df = pd.concat([tdf, self.df])
-        self.df.to_csv('log.csv', index=False)
+        self.df.to_csv(filename, index=False)
